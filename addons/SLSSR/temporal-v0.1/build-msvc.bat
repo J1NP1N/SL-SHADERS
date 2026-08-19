c000a9fd8d57ab4c75fd5f346aa58abc9fee4707
J1NP1N <130268287+J1NP1N@users.noreply.github.com>
2026-08-19 17:15:20 +0900

    Add temporal interop build script

---
+++ addons/SLSSR/temporal-v0.1/build-msvc.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+if "%~1"=="" (
+  echo Usage: build-msvc.bat ^<ReShadeRoot^>
+  exit /b 2
+)
+set "RESHADEROOT=%~1"
+if not exist "%RESHADEROOT%\include\reshade.hpp" (
+  echo ERROR: reshade.hpp not found at "%RESHADEROOT%\include\reshade.hpp"
+  exit /b 3
+)
+if not exist build mkdir build
+cl /nologo /std:c++17 /EHsc /O2 /MD /LD /DWIN32_LEAN_AND_MEAN /DNOMINMAX /I"%RESHADEROOT%\include" SLSSRTemporalLink.cpp /link /OUT:build\SLSSRTemporalLink.addon
+if errorlevel 1 exit /b %errorlevel%
+echo Built build\SLSSRTemporalLink.addon
+endlocal
